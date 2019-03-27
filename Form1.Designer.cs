0c5936d8ed561935442a8a7c5218b5f24becc353
vedha gnanam <vetha.gnanam98@gmail.com>
2019-03-27 23:53:08 +0530

    added basic ui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(48, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "select file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(185, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "encrypt file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +18,46 @@ namespace VideoEncryption
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EncryptFile(openFileDialog1.FileName, Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        }
+            private const string SKey = "_?73^?dVT3st5har3";
+        private const string SaltKey = "!2S@LT&KT3st5har3EY";
+        private const int Iterations = 1042; // Recommendation is >= 1000
+        
+        public static void EncryptFile(string srcFilename, string destFilename)
+        {
+            var aes = new AesManaged();
+            aes.BlockSize = aes.LegalBlockSizes[0].MaxSize;
+            aes.KeySize = aes.LegalKeySizes[0].MaxSize;
+            var salt = Encoding.UTF7.GetBytes(SaltKey);
+            FileStream fs = new FileStream("EncryptedFile.txt", FileMode.Create, FileAccess.Write);
+            var key = new Rfc2898DeriveBytes(SKey, salt, Iterations);
+            aes.Key = key.GetBytes(aes.KeySize / 8);
+            aes.IV = key.GetBytes(aes.BlockSize / 8);
+            aes.Mode = CipherMode.CBC;
+            ICryptoTransform transform = aes.CreateEncryptor(aes.Key, aes.IV);
+            using (var dest = new FileStream(destFilename, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+            {
+                using (var cryptoStream = new CryptoStream(fs, transform, CryptoStreamMode.Write))
+                {
+                    using (var source = new FileStream(srcFilename, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        source.CopyTo(cryptoStream);
+                    }
+                }
+            }
+        }
     }
 }
+    
+    
+
+
