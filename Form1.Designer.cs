16bcc7970da68c51315f22b18e51dc94a1d61f97
vedha gnanam <vetha.gnanam98@gmail.com>
2019-03-28 17:55:43 +0530

    added login form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -80,7 +80,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Encryptor";
             this.ResumeLayout(false);
 
         }
