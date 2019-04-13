f0cc389437ddfa67c1f62cfce6de2108432c55d7
vedha gnanam <vetha.gnanam98@gmail.com>
2019-04-13 15:00:13 +0530

    added server and middlewares

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log('connected');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('err');
+});
+
+const app = express();
+const users = require('./routes/users');
+
+const port = process.env.PORT || 8080;
+
+
+app.use(cors());
+//static folders
+app.use(express.static(path.join(__dirname, 'client')));
+
+//body parser middleware
+app.use(bodyParser.json());
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+app.use('/users', users);
+
+app.get("/", (req, res) => {
+    res.send("fnfffn");
+});
+app.get("*", (req, res) => {
+    res.redirect('/');
+});
+
+
+app.listen(port, () => {
+    console.log("running on port 3000");
+});
